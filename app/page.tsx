69331c20789421076376412229e712112203d462
Tsurielvizel <tsurielvizel@campus.technion.ac.il>
2025-03-09 20:28:24 +0200

    client console log

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Hello from "@/app/components/hello";
 export default function Home() {
   console.log('what am i doing here?');
 
   return (
+    <> 
     <h1 className="text-3xl">Welcome to Next.js</h1>
+    <Hello />
+    </>
   );
 }
