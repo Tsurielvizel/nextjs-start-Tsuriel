7a7fc1684788d60ec1c06ec46aed42c90d229904
Tsurielvizel <tsurielvizel@campus.technion.ac.il>
2025-03-09 21:04:53 +0200

    Update hello.tsx

--- app/components/hello.tsx
+++ app/components/hello.tsx
@@ -4,8 +4,8 @@ function Hello() {
     console.log('i am client component');
   return (
     <div>
-        <h1>Hello im a hello func</h1>
-    </div>
+        <h1 style={{ display: 'inline-block', whiteSpace: 'nowrap' }}>Hello im a hello func</h1>
+        </div>
   );
 }
 
