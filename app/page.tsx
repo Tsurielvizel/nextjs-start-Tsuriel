d6df8b5f9caf2cf46439165b76e862ee098a3fc5
Tsurielvizel <tsurielvizel@campus.technion.ac.il>
2025-03-09 20:15:22 +0200

    some css and page text changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
+  console.log('what am i doing here?');
+
   return (
     <h1 className="text-3xl">Welcome to Next.js</h1>
   );
